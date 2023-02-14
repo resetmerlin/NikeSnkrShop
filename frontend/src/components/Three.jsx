57c24f67d1c50f0456347c1c62a12d0e0086af31
resetmerlin <108568153+resetmerlin@users.noreply.github.com>
2023-02-14 21:30:03 +0900

    Update Three.jsx

--- frontend/src/components/Three.jsx
+++ frontend/src/components/Three.jsx
@@ -35,7 +35,7 @@ const Three = ({ model }) => {
           ) : model == 6 ? (
             <NikeDunkLow open={open} setOpen={setOpen} />
           ) : (
-            <span>3d error load</span>
+            "3d error load"
           )}
 
           <Floor />
